85779d0dd9cf2d5ab77a67a889442ec5e1e03224
rcucho <38509900+rcucho@users.noreply.github.com>
2022-01-20 11:51:57 -0500

    Add files via upload

---
+++ currency_menu/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Moneda - Menú',
+    'version': '14.0.1.0.0',
+    'author': 'Mouse Technologies',
+    'category': 'Technical Configuration',
+    'summary': 'Creación de Menú Principal de Monedas',
+    'license': 'LGPL-3',
+    'website': 'https://www.mstech.pe',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_currency_views.xml',
+    ],
+    'installable': True,
+}
